bdfc43d8ff747ad782bb504e2ec4b4ed70d985bd
Aditya <addiittya2006@gmail.com>
2016-06-10 17:17:22 +0530

    got the admin portal working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   # before_filter :configure_permitted_parameters, if: :devise_controller?
+  # before_filter :configure_update_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to articles_url, :alert => exception.message
@@ -11,7 +13,11 @@ class ApplicationController < ActionController::Base
   protected
 
   # def configure_permitted_parameters
-    # devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
+  #   devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
+  # end
+
+  # def configure_update_parameters
+  #   devise_parameter_sanitizer.for(:update) {|u| u.permit(:name, :email, :password, :password_confirmation, :writer, :admin, :commit)}
   # end
 
 end
