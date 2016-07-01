ddfa68f068c07901509272275a29c9294b022f91
Aditya <addiittya2006@gmail.com>
2016-07-02 05:17:35 +0530

    complete feature usage #20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
 
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to articles_url, :alert => exception.message
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Feature.create([{name: 'Permmissions Scanner', count: 0}, {name: 'Settings Checker', count: 0}, {name: 'Blocker Logs', count: 0}, {name: 'Application Lock', count: 0}, {name: 'Service Lock', count: 0}, {name: 'Geo-fencing', count: 0}, {name: 'Knowledge Base', count: 0}])
