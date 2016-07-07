674f6fd1a1372036287bf38a4b1be064fed1838d
Aditya <addiittya2006@gmail.com>
2016-07-07 16:45:23 +0530

    correct views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to articles_url, :alert => exception.message
