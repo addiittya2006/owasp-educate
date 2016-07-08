6e84a95e0c1d4359ef48454de0bdaf83df152ccc
Aditya <addiittya2006@gmail.com>
2016-07-08 18:25:44 +0530

    implement for features

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,4 +2,12 @@ class Feature < ActiveRecord::Base
 
   has_many :usages, :as => :usable
 
+  def range_count(start_params, end_params)
+    start_date = DateTime.new(start_params['date(1i)'].to_i, start_params['date(2i)'].to_i, start_params['date(3i)'].to_i)
+    end_date = DateTime.new(end_params['date(1i)'].to_i, end_params['date(2i)'].to_i, end_params['date(3i)'].to_i)
+    usages.where("created_at < ? AND created_at > ?", end_date, start_date).size
+  rescue ArgumentError
+    usages.count
+  end
+
 end
